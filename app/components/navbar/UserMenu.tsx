329dfa3f718094f960cd7a6bd6cc7f2371d9117d
Dilantha Wijesinghe <wdilantha66@gmail.com>
2023-10-28 19:55:25 +0530

    add user menu to navbar

---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div 
+          onClick={() => {}}
+          className="hidden px-4 py-3 text-sm font-semibold transition rounded-full cursor-pointer md:block hover:bg-neutral-100">
+          Airbnb your home
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserMenu
