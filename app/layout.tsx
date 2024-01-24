ab41ebfa29799681e620d7e3cdf3e41a72a9636d
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-01-24 22:22:56 +0530

    feat: Modal and button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
         <ClientOnly>
-          <Modal isOpen title="Hello world" />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
 
